de30d15da27a63be10c7ca54eb24653d070bb7cd
HeldEnderMen <ajantefraser13@gmail.com>
2024-01-01 10:51:03 -0400

    Geothermal Map Complete with Legend!

--- Geothermal/script.js
+++ Geothermal/script.js
@@ -1,86 +1,36 @@
-$(function() {
+$(document).ready(function() {
   var map = L.map('map').setView([17.087635, -61.772346], 13);
 
   // Tile layer
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    attribution: '&copy; DisasterReady Caribbean'
   }).addTo(map);
 
   var layers = {
     temperature: L.tileLayer('https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=f0432aedfec8f9aa7b07f0b4472222de', {
-      attribution: '&copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+      attribution: '&copy; DisasterReady Caribbean',
       APIkey: 'f0432aedfec8f9aa7b07f0b4472222de'
     }),
     precipitation: L.tileLayer('https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=f0432aedfec8f9aa7b07f0b4472222de', {
-      attribution: '&copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+      attribution: '&copy; DisasterReady Caribbean',
       APIkey: 'f0432aedfec8f9aa7b07f0b4472222de'
     }),
     wind: L.tileLayer('https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=f0432aedfec8f9aa7b07f0b4472222de', {
-      attribution: '&copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+      attribution: '&copy; DisasterReady Caribbean',
       APIkey: 'f0432aedfec8f9aa7b07f0b4472222de'
     }),
     clouds: L.tileLayer('https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=f0432aedfec8f9aa7b07f0b4472222de', {
-      attribution: '&copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+      attribution: '&copy; DisasterReady Caribbean',
       APIkey: 'f0432aedfec8f9aa7b07f0b4472222de'
     }),
     pressure: L.tileLayer('https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=f0432aedfec8f9aa7b07f0b4472222de', {
-      attribution: '&copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+      attribution: '&copy; DisasterReady Caribbean',
       APIkey: 'f0432aedfec8f9aa7b07f0b4472222de'
     })
   };
 
-  // Default Layer [Temperature]
+  // Default Layer: Temperature
   layers.temperature.addTo(map);
-
-  // Function to Legend from Checkbox
-  function updateLegends() {
-    if ($('#temperature-layer').is(':checked')) {
-      $('#temperature-legend').show();
-    } else {
-      $('#temperature-legend').hide();
-    }
-
-    if ($('#precipitation-layer').is(':checked')) {
-      $('#precipitation-legend').show();
-    } else {
-      $('#precipitation-legend').hide();
-    }
-
-    if ($('#wind-layer').is(':checked')) {
-      $('#wind-legend').show();
-    } else {
-      $('#wind-legend').hide();
-    }
-
-    if ($('#clouds-layer').is(':checked')) {
-      $('#clouds-legend').show();
-    } else {
-      $('#clouds-legend').hide();
-    }
-
-    if ($('#pressure-layer').is(':checked')) {
-      $('#pressure-legend').show();
-    } else {
-      $('#pressure-legend').hide();
-    }
-  }
-
-  // Function to Update Legend
-  function updateLegendContent() {
-    $('#temperature-legend').html('Temperature Scale: Add temperature scale content here');
-    $('#precipitation-legend').html('Precipitation Scale: Add precipitation scale content here');
-    $('#wind-legend').html('Wind Speed Scale: Add wind speed scale content here');
-    $('#clouds-legend').html('Clouds Scale: Add clouds scale content here');
-    $('#pressure-legend').html('Sea Level Pressure Scale: Add sea level pressure scale content here');
-  }
-
-  // Check to see if checkbox changed
-  $('.layer-checkbox input[type="checkbox"]').change(function() {
-    updateLegends();
-  });
-
-  // Runs the Update Function
-  updateLegendContent();
 
   //Layers
   L.control.layers({}, {
@@ -93,6 +43,30 @@ $(function() {
     position: 'topright'
   }).addTo(map);
 
-  // Run Updated Layers Functin
-  // updateLayers();
+  // Current Layer
+  function getActiveLayer() {
+    for (var layerName in layers) {
+      if (map.hasLayer(layers[layerName])) {
+        return layerName;
+      }
+    }
+    return null;
+  }
+
+  // HTML for Current layer
+  function loadLegend() {
+    var activeLayer = getActiveLayer();
+    if (activeLayer) {
+      var legendHTMLPath = './legends/' + activeLayer.toLowerCase() + '-legend.html';
+      $('.legend').load(legendHTMLPath);
+    }
+  }
+
+  // Run the function at the start of the program for Temperature
+  loadLegend();
+
+  // Every time the box is clicked it updates The current layer!
+  $('.leaflet-control-layers-selector').change(function() {
+    loadLegend();
+  });
 });
